a936068e4d72ef963ba785b3f46187b2a34a9610
Malcolm Breckenridge <breckenridmj@vcu.edu>
2023-09-22 00:07:24 -0500

    Update darkpool example values to fit stock data

--- components/Tables&Graphs/DarkPool.tsx
+++ components/Tables&Graphs/DarkPool.tsx
@@ -7,19 +7,19 @@ import Table from '@mui/material/Table';
 import Paper from '@mui/material/Paper';
 import TableBody from '@mui/material/TableBody';
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-    return { name, calories, fat, carbs, protein };
+function createData(TIMESTAMP: string, PRICE: number, SIZE: number, SENTIMENT: string, VALUE: string) {
+    return { TIMESTAMP, PRICE, SIZE, SENTIMENT, VALUE };
   }
 
 const DarkPool = () => {
     
     
     const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
+        createData('09:48:15 AM', 438.86, 2544413, 'Above Ask', '$111,576,993.52'),
+        createData('09:48:15 AM', 438.86, 2544413, 'Above Ask', '$111,576,993.52'),
+        createData('09:48:15 AM', 438.86, 2544413, 'Above Ask', '$111,576,993.52'),
+        createData('09:48:15 AM', 438.86, 2544413, 'Above Ask', '$111,576,993.52'),
+        createData('09:48:15 AM', 438.86, 2544413, 'Above Ask', '$111,576,993.52'),
       ];
 
 
@@ -29,11 +29,11 @@ const DarkPool = () => {
                 <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            <TableCell>TIMESTAMP</TableCell>
+                            <TableCell align="right">PRICE</TableCell>
+                            <TableCell align="right">SIZE&nbsp;</TableCell>
+                            <TableCell align="right">SENTIMENT&nbsp;</TableCell>
+                            <TableCell align="right">VALUE&nbsp;</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -43,12 +43,12 @@ const DarkPool = () => {
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <TableCell component="th" scope="row">
-                                    {row.name}
+                                    {row.TIMESTAMP}
                                 </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                                <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
+                                <TableCell align="right">{row.PRICE}</TableCell>
+                                <TableCell align="right">{row.SIZE}</TableCell>
+                                <TableCell align="right">{row.SENTIMENT}</TableCell>
+                                <TableCell align="right">{row.VALUE}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
